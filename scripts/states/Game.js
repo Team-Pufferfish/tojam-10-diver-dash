3ac21e215d72cb512cc433cb3f4497d97b59a3c3
furrot <sfrerotte@gmail.com>
2015-05-01 18:09:56 -0400

    Added basic light support for objects.

--- scripts/states/Game.js
+++ scripts/states/Game.js
@@ -30,11 +30,24 @@ var Game = (function (_super) {
         var result = this.findObjectsByType('playerStart', this.map, 'Objects');
         this.player = this.game.add.sprite(result[0].x, result[0].y, 'player');
         this.game.physics.arcade.enable(this.player);
-        this.player.body.setSize(21, 28, 5, 2);
+        this.player.body.setSize(21, 28);
+        this.player.anchor.setTo(0.5, 0.5);
         //the camera will follow the player in the world
         this.game.camera.follow(this.player);
         //move player with cursor keys
         this.cursors = this.game.input.keyboard.createCursorKeys();
+        //Set up lights
+        this.LIGHT_RADIUS = 100;
+        // Create the shadow texture
+        this.shadowTexture = this.game.add.bitmapData(this.game.width, this.game.height);
+        // Draw shadow
+        // Create an object that will use the bitmap as a texture
+        this.lightSprite = this.game.add.image(0, 0, this.shadowTexture);
+        // Set the blend mode to MULTIPLY. This will darken the colors of
+        // everything below this sprite.
+        this.lightSprite.blendMode = PIXI.blendModes.MULTIPLY;
+        this.lights = this.game.add.group();
+        this.lights.add(this.player);
     };
     Game.prototype.update = function () {
         //collision
@@ -45,17 +58,19 @@ var Game = (function (_super) {
         this.player.body.velocity.y = 0;
         this.player.body.velocity.x = 0;
         if (this.cursors.up.isDown) {
-            this.player.body.velocity.y -= 50;
+            this.player.body.velocity.y -= 150;
         }
         else if (this.cursors.down.isDown) {
-            this.player.body.velocity.y += 50;
+            this.player.body.velocity.y += 150;
         }
         if (this.cursors.left.isDown) {
-            this.player.body.velocity.x -= 50;
+            this.player.body.velocity.x -= 150;
         }
         else if (this.cursors.right.isDown) {
-            this.player.body.velocity.x += 50;
+            this.player.body.velocity.x += 150;
         }
+        this.updateLights();
+        console.log("player1x:" + this.player.x + " player1y:" + this.player.y + " Light:" + this.lightSprite.x + "/" + this.lightSprite.y);
     };
     Game.prototype.createItems = function () {
         //create items
@@ -117,6 +132,37 @@ var Game = (function (_super) {
         if (tile.index == 26) {
             console.log("Ouch!");
         }
+    };
+    Game.prototype.updateLights = function () {
+        // This function updates the shadow texture (this.shadowTexture).
+        // First, it fills the entire texture with a dark shadow color.
+        // Then it draws a white circle centered on the pointer position.
+        // Because the texture is drawn to the screen using the MULTIPLY
+        // blend mode, the dark areas of the texture make all of the colors
+        // underneath it darker, while the white area is unaffected.
+        //move lightspirte
+        // Draw shadow
+        this.shadowTexture.context.fillStyle = 'rgb(0, 0, 0)';
+        this.shadowTexture.context.fillRect(0, 0, this.game.width, this.game.height);
+        // Iterate through each of the lights and draw the glow
+        this.lights.forEach(function (light) {
+            // Randomly change the radius each frame
+            var radius = this.LIGHT_RADIUS + this.game.rnd.integerInRange(1, 10);
+            var screenX = light.x - this.lightSprite.x;
+            var screenY = light.y - this.lightSprite.y;
+            // Draw circle of light with a soft edge
+            var gradient = this.shadowTexture.context.createRadialGradient(screenX, screenY, this.LIGHT_RADIUS * 0.75, screenX, screenY, radius);
+            gradient.addColorStop(0, 'rgba(255, 255, 255, 1.0)');
+            gradient.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
+            this.shadowTexture.context.beginPath();
+            this.shadowTexture.context.fillStyle = gradient;
+            this.shadowTexture.context.arc(screenX, screenY, radius, 0, Math.PI * 2);
+            this.shadowTexture.context.fill();
+        }, this);
+        this.lightSprite.x = this.camera.x;
+        this.lightSprite.y = this.camera.y;
+        // This just tells the engine it should update the texture cache
+        this.shadowTexture.dirty = true;
     };
     return Game;
 })(Phaser.State);
