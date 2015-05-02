de5488e861e3e936cd0a46d0ce2e459a805da9ca
Michael Yabko <michael.yabko@sharppointdata.com>
2015-05-01 20:04:31 -0400

    mergerered stephens changes.

--- scripts/states/Game.ts
+++ scripts/states/Game.ts
@@ -17,9 +17,16 @@ class Game extends Phaser.State {
     mapLayer:Phaser.TilemapLayer;
     hazardLayer:Phaser.TilemapLayer;
     items:Phaser.Group;
+    decorations:Phaser.Group;
     doors:Phaser.Group;
     cursors:Phaser.CursorKeys;
     player: Player;
+
+    //Lighting model
+    lights : Phaser.Group;
+    shadowTexture : Phaser.BitmapData;
+    lightSprite : Phaser.Image;
+    LIGHT_RADIUS:number;
 
     constructor() {
         super();
@@ -35,45 +42,71 @@ class Game extends Phaser.State {
         //create layer
         this.mapLayer = this.map.createLayer('Tiles');
 
-        this.map.setCollisionByExclusion([1,2,3,46,47,56]);
-
-        //resizes the game world to match the layer dimensions
+        this.map.setCollisionByExclusion([1,2,3,46,47,56],true,"Tiles");
         this.mapLayer.resizeWorld();
 
         this.createItems();
+        this.createDecorations();
         this.createDoors();
 
         //create player
         var result = this.findObjectsByType('playerStart', this.map, 'Objects');
-       // this.player = this.game.add.sprite(result[0].x, result[0].y, 'player');
-        this.player = new Player(result[0].x,result[0].y,this.game,this.game.input.gamepad.pad1);
 
-        //this.player.body.setSize(10, 14, 2, 1);
+        this.player = new Player(result[0].x,result[0].y,this.game,this.game.input.gamepad.pad1);
+        this.player.sprite.body.setSize(21, 28);
+
 
         //the camera will follow the player in the world
         this.game.camera.follow(this.player.sprite);
 
         //move player with cursor keys
         this.cursors = this.game.input.keyboard.createCursorKeys();
+
+        //Set up lights
+        this.LIGHT_RADIUS = 120;
+        // Create the shadow texture
+        this.shadowTexture = this.game.add.bitmapData(this.game.width, this.game.height);
+        // Draw shadow
+        // Create an object that will use the bitmap as a texture
+        this.lightSprite = this.game.add.image(0, 0, this.shadowTexture);
+
+        // Set the blend mode to MULTIPLY. This will darken the colors of
+        // everything below this sprite.
+        this.lightSprite.blendMode = PIXI.blendModes.MULTIPLY;
+
+        this.lights = this.game.add.group();
+        this.lights.add(this.player.sprite);
     }
 
     update() {
         //collision
-        this.game.physics.arcade.collide(this.player.sprite, this.mapLayer);
+
+        this.game.physics.arcade.collide(this.player.sprite, this.mapLayer, this.environmentCollision,null,this);
         this.game.physics.arcade.overlap(this.player.sprite, this.items, this.collect, null, this);
         this.game.physics.arcade.overlap(this.player.sprite, this.doors, this.enterDoor, null, this);
 
         this.player.update();
+
+        this.updateLights();
     }
 
     createItems() {
         //create items
         this.items = this.game.add.group();
         this.items.enableBody = true;
-        var item;
         var result = this.findObjectsByType('item', this.map, 'Objects');
         result.forEach(function (element) {
             this.createFromTiledObject(element, this.items);
+        }, this);
+    }
+
+    createDecorations() {
+        //create items
+        this.decorations = this.game.add.group();
+        this.decorations.enableBody = true;
+        var result = this.findObjectsByType('decoration', this.map, 'Objects');
+        result.forEach(function (element) {
+            this.createFromTiledObject(element, this.decorations);
         }, this);
     }
 
@@ -123,5 +156,52 @@ class Game extends Phaser.State {
 
     private enterDoor(player, door) {
         console.log('entering door that will take you to ' + door.targetTilemap + ' on x:' + door.targetX + ' and y:' + door.targetY);
+    }
+
+    private environmentCollision(player, tile) {
+        if (tile.index == 26){
+            console.log("Ouch!");
+        }else if (tile.index == 36 || tile.index == 37){
+            console.log("Escaped!!!");
+        }
+    }
+
+    updateLights(){
+        // This function updates the shadow texture (this.shadowTexture).
+        // First, it fills the entire texture with a dark shadow color.
+        // Then it draws a white circle centered on the pointer position.
+        // Because the texture is drawn to the screen using the MULTIPLY
+        // blend mode, the dark areas of the texture make all of the colors
+        // underneath it darker, while the white area is unaffected.
+        //move lightspirte
+
+        // Draw shadow
+        this.shadowTexture.context.fillStyle = 'rgb(0, 0, 0)';
+        this.shadowTexture.context.fillRect(0, 0, this.game.width, this.game.height);
+
+        // Iterate through each of the lights and draw the glow
+        this.lights.forEach(function(light) {
+            // Randomly change the radius each frame
+            var radius = this.LIGHT_RADIUS + this.game.rnd.integerInRange(1,10);
+            var screenX = light.x - this.lightSprite.x;
+            var screenY = light.y - this.lightSprite.y;
+            // Draw circle of light with a soft edge
+            var gradient =
+                this.shadowTexture.context.createRadialGradient(
+                    screenX, screenY,this.LIGHT_RADIUS * 0.25,
+                    screenX, screenY, radius);
+            gradient.addColorStop(0, 'rgba(255, 255, 255, 1.0)');
+            gradient.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
+
+            this.shadowTexture.context.beginPath();
+            this.shadowTexture.context.fillStyle = gradient;
+            this.shadowTexture.context.arc(screenX, screenY, radius, 0, Math.PI*2);
+            this.shadowTexture.context.fill();
+        }, this);
+        this.lightSprite.x = this.camera.x;
+        this.lightSprite.y = this.camera.y;
+
+        // This just tells the engine it should update the texture cache
+        this.shadowTexture.dirty = true;
     }
 }
