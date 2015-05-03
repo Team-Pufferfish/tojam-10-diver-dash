ed6706afab7fc7e5e9acbf6fe1f9aacbe970219b
furrot <sfrerotte@gmail.com>
2015-05-03 16:55:59 -0400

    Function score screen with very basic UI.

--- scripts/states/Scores.ts
+++ scripts/states/Scores.ts
@@ -8,6 +8,12 @@ class Scores extends Phaser.State {
     level: number;
     maxLevel: number = 2;
 
+    scoreTitle: Phaser.Text;
+    scoreSummary: Phaser.Text;
+
+    scoreTeam: Phaser.Text;
+    scoreGuide: Phaser.Text;
+
     init(gameState){
         this.gameState = gameState;
     }
@@ -20,25 +26,54 @@ class Scores extends Phaser.State {
         console.log("Hey you won or you all died");
         var playerCount = 1;
         var aVictory = false;
+        var title = "";
+        var summary = "";
+        var guide = "";
+
         this.gameState.playerDeaths.forEach(function(playerDeath){
-            if (!playerDeath.isVictorius)
-                console.log("Player "+ playerCount + " died of " + playerDeath.reason + " at " + playerDeath.time);
-            else {
+            if (!playerDeath.isVictorius) {
+                console.log("Player " + playerCount + " died of " + playerDeath.reason + " after " + playerDeath.time);
+                summary += "Player "+ playerCount + " died of " + playerDeath.reason + " after " + playerDeath.time + "s\n";
+            }else {
                 console.log("Player " + playerCount + " escaped with " + playerDeath.gold + " gold in " + playerDeath.time);
+                summary += "Player " + playerCount + " escaped with " + playerDeath.gold + " gold in " + playerDeath.time + "s\n";
                 this.gameState.teamScore += playerDeath.gold;
                 aVictory = true;
             }
             playerCount++;
         },this);
 
-
         if (aVictory) {
             console.log("Level Complete Team Score: " + this.gameState.teamScore);
-            this.game.time.events.add(3000,this.startGame,this);
+            title = "Level " + this.gameState.level + " Complete!";
+            this.game.time.events.add(5000,this.startGame,this);
         }else{
             console.log("You Lose! Team Score: " + this.gameState.teamScore);
-            this.game.time.events.add(3000,this.restartGame,this);
+            title = "Level "+this.gameState.level+" Failed!";
+            this.game.time.events.add(5000,this.restartGame,this);
         }
+
+        if (this.gameState.level+1 <= this.maxLevel && aVictory) {
+            guide = "Good luck on the next level!";
+        }else{
+            guide = "Game over, thanks for playing!"
+        }
+
+        this.scoreTitle = this.game.add.text(this.game.width/2,this.game.height/2-300,title, {font: '72px Arial', fill:'#ffffff'});
+        this.scoreTitle.fixedToCamera = true;
+        this.scoreTitle.anchor.setTo(0.5,0.5);
+
+        this.scoreSummary = this.game.add.text(this.game.width/2,this.game.height/2-100,summary, {font: '48px Arial', fill:'#ffffff'});
+        this.scoreSummary.fixedToCamera = true;
+        this.scoreSummary.anchor.setTo(0.5,0.5);
+
+        this.scoreTeam = this.game.add.text(this.game.width/2,this.game.height/2+100,"Team Score: " +this.gameState.teamScore, {font: '48px Arial', fill:'#ffffff'});
+        this.scoreTeam.fixedToCamera = true;
+        this.scoreTeam.anchor.setTo(0.5,0.5);
+
+        this.scoreGuide= this.game.add.text(this.game.width/2,this.game.height/2+200,guide, {font: '48px Arial', fill:'#ffffff'});
+        this.scoreGuide.fixedToCamera = true;
+        this.scoreGuide.anchor.setTo(0.5,0.5);
     }
 
     private startGame() {
